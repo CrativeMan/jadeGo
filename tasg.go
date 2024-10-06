ae5de8ec38b6f7349be4f9c74350bce1c1b29c7b
crative <hannig.sebi@gmail.com>
2024-10-06 21:25:14 +0200

    asasdd

--- tasg.go
+++ tasg.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	home, _ := homedir.Dir()
 
-	dbPath := filepath.Join(home, "tasks.db")
+	dbPath := filepath.Join(home, ".tasks.db")
 	must(db.Init(dbPath))
 
 	must(cmd.RootCmd.Execute())
